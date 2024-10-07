a9b58bde2f08580bb7b97efb7c80c9335e42f8ce
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2024-10-07 19:27:42 -0400

    I created my first program

---
+++ HelloWorld/src/main/java/frc/robot/Main.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+
+//import edu.wpi.first.wpilibj.RobotBase;
+
+/**
+ * Do NOT add any static variables to this class, or any initialization at all.
+ * Unless you know what
+ * you are doing, do not modify this file except to change the parameter class
+ * to the startRobot
+ * call.
+ */
+public final class Main {
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+  private Main() {
+  }
+
+  /**
+   * Main initialization function. Do not perform any initialization here.
+   *
+   * <p>
+   * If you change your main robot class, change the parameter type.
+   */
+  public static void main(String... args) {
+    // RobotBase.startRobot(Robot::new);
+    for(int i = 0; i < 50; i++) {
+      print(i,"Hello World :)! {}");
+    }
+    System.out.print("////////////////////////////////////////////////////////////////////////");
+    boolean doLoop= true;
+    int a = 0;
+    while (doLoop == true) {
+      print(a, "Im Doing It!");
+      a += 2;
+      if (a > 100) {
+        doLoop = false;
+      }
+    }
+      System.out.print("////////////////////////////////////////////////////////////////////////");
+      do {
+        print(a, "HOW MANY TIMES! ");
+      }while(doLoop);
+  }
+
+  private static void print(int count, String string) {
+    LOG.error(string + count);
+  }
+}
