191e249f2c62f04f3abf1cf12e046c30fa990b53
Max <imautuber20@gmail.com>
2024-10-10 18:13:55 -0400

    d

--- HelloWorld/src/main/java/frc/robot/shapes/Circle.java
+++ HelloWorld/src/main/java/frc/robot/shapes/Circle.java
@@ -2,16 +2,17 @@ package frc.robot.shapes;
 
 public class Circle implements Shape {
 
+    private double radius = 0;
     @Override
     public double perimiter() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'perimiter'");
+        return 0;
     }
 
     @Override
     public double area() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'area'");
+        return 0;
     }
     
 }
