f692dccc0916d2a701c13fa5e9473617be522641
vijay_n7 <vijaynannapuraju@gmail.com>
2024-10-17 19:31:57 -0400

    Added (to, in some cases) some Unit tests. (problems)

--- HelloWorld/src/main/test/java/frc/robot/shapes/CircleTest.java
+++ HelloWorld/src/main/test/java/frc/robot/shapes/CircleTest.java
@@ -4,5 +4,19 @@
 
 package frc.robot.shapes;
 
+import java.beans.Transient;
+
 /** Add your docs here. */
-public class CircleTest {}
+public class CircleTest {
+    @Test
+    public void circleArea() {
+        Circle circleArea = new Circle (8);
+        assertEquals (Math.PI * Math.pow(8, 2), circleArea.area(), "Expected should match actual");
+    }
+
+    @Test
+    public void circlePerimeter() {
+        Circle circlePerimeter = new Circle (8);
+        assertEquals (Math.PI * (8 * 2), circlePerimeter.perimiter(), "Expected should match actual")
+    }
+}
--- HelloWorld/src/main/test/java/frc/robot/shapes/RectangleTest.java
+++ HelloWorld/src/main/test/java/frc/robot/shapes/RectangleTest.java
@@ -7,11 +7,19 @@ package frc.robot.shapes;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.beans.Transient;
+
 /** Add your docs here */
 public class RectangleTest {
     @Test
     public void testArea() {
         Rectangle rectangle = new Rectangle (4, 3);
-        assertEquals(12.1, rectangle.getArea(), "Expected and actual should")
+        assertEquals(12.0, rectangle.getArea(), "Expected and actual should match")
     }
+    @Test 
+    public void testPerimeter(){
+        Rectangle rectangle = new Rectangle (3, 4);
+        assertEquals(14.0, rectangle.perimeter(),"Expected and actual should match")
+    }
+
 }
