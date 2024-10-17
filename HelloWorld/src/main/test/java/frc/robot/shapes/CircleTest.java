1ec64e0b34aaa3f32f8d4ab209ddca7478a812e5
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2024-10-17 18:52:08 -0400

    Programmed to find out area and perimeter

--- HelloWorld/src/main/test/java/frc/robot/shapes/CircleTest.java
+++ HelloWorld/src/main/test/java/frc/robot/shapes/CircleTest.java
@@ -1,8 +1,22 @@
-// Copyright (c) FIRST and other WPILib contributors.
+
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.shapes;
 
+import java.beans.Transient;
+
+import org.junit.jupiter.api.Test;
+
 /** Add your docs here. */
-public class CircleTest {}
+public class CircleTest {
+    @Test
+    public void circleArea(){
+        Circle circleArea = new Circle(8);
+        assertEquals(Math.PI * Math.pow(8,2), circleArea, "Expected should match actual");
+    }
+    @Test
+    public void circlePerimeter(){
+
+    }
+}
--- HelloWorld/src/main/test/java/frc/robot/shapes/RectangleTest.java
+++ HelloWorld/src/main/test/java/frc/robot/shapes/RectangleTest.java
@@ -7,11 +7,18 @@ package frc.robot.shapes;
 import org.junit.jupiter.api.Test;
 import static org.jupiter.api.Assertions.assertEquals;
 
+import java.beans.Transient;
+
 /** Add your docs here. */
 public class RectangleTest {
     @Test 
     public void testArea() {
-        Rectangle rectangle = new Rectangle (4, 3);
-        assertEquals(expected: 12.0, rectangle.getArea(), message: "Expected and actual");
+        Rectangle rectangle = new Rectangle (length = 4, width = 3);
+        assertEquals(12.0, rectangle.area(), "Expected and actual");
+    }
+    @Test
+    public void testPerimeter(){
+        Rectangle rectangle = new Rectangle(4, 3);
+        
     }
 }
