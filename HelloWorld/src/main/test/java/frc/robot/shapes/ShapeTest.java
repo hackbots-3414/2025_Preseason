59de1ec62a982456683f0263f5764245f7a4000e
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2024-10-24 19:25:21 -0400

    Created ShapeTest

--- HelloWorld/src/main/test/java/frc/robot/shapes/ShapeTest.java
+++ HelloWorld/src/main/test/java/frc/robot/shapes/ShapeTest.java
@@ -4,9 +4,35 @@
 
 package frc.robot.shapes;
 
+import org.junit.jupiter.api.Test;
+
 /** Add your docs here. */
 public class ShapeTest {
     private static final int MAX_NUMBER_SHAPES = 10;
-    private Shape[] shapesArray = null;
+    private Shape[] shapesArray = new Shape[MAX_NUMBER_SHAPES];
 
+    @Test
+    public void testShapes() {
+        for (int i = 0; i < shapesArray.length; i++) {
+            switch (i % 3) {
+                case 1:
+                    shapesArray[i] = new Circle(5);
+                    break;
+
+                case 2:
+                    shapesArray[i] = new Rectangle(3.0, 6);
+                    break;
+
+                default:
+                    shapesArray[i] = new Square();
+                    ((Square) shapesArray[i]).setLength(4);
+            }
+            shapesArray[i] = new Circle(5);
+
+        }
+        for (int i = 0; i < shapesArray.length; i++) {
+            assertTrue(shapesArray[i].area() > 0);
+
+        }
+    }
 }
