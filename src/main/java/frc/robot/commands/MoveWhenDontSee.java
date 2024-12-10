32e4564208d341c642ee25afd86c210797592ed4
MC Ride <hackbots3414@gmail.com>
2024-12-09 19:25:41 -0500

    finished up command movewhenpathnotclear and shortened code

--- src/main/java/frc/robot/commands/MoveWhenDontSee.java
+++ src/main/java/frc/robot/commands/MoveWhenDontSee.java
@@ -4,14 +4,42 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.LazerRangeFinder;
 
 /** Add your docs here. */
-public class MoveWhenDontSee {
+public class MoveWhenDontSee extends Command{
+
     private CommandSwerveDrivetrain drivetrain;
-public MoveWhenDontSee(CommandSwerveDrivetrain drivetrain, CommandLazerRangeFinder lazerRangeFinder) {
-    this.drivetrain = drivetrain;
-    this.lazerRangeFinder = lazerRangeFinder;
-}
+    private LazerRangeFinder lazerRangeFinder;
+
+    public MoveWhenDontSee(CommandSwerveDrivetrain drivetrain, LazerRangeFinder lazerRangeFinder) {
+        this.drivetrain = drivetrain;
+        this.lazerRangeFinder = lazerRangeFinder;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        // TODO Auto-generated method stub
+        super.end(interrupted);
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        super.execute();
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+        super.initialize();
+    }
+
+    @Override
+    public boolean isFinished() {
+        // TODO Auto-generated method stub
+        return super.isFinished();
+    }
 }
